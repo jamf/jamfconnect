f4b59db2754ee466a13fbb9510e3c90734da4dfe
Sean Rabbitt <43422108+sean-rabbitt@users.noreply.github.com>
2023-04-04 11:07:35 -0700

    Update Jamf_Connect_LastSignIn.sh
    For any Smart Computer Group built on "before X date", if you get a return of "Does not exist" instead, you'll break the group.
    
    Now returns epoch 0 instead of a string to avoid problems calculating the smart computer groups.

--- built_in_extension_attributes/Jamf_Connect_LastSignIn.sh
+++ built_in_extension_attributes/Jamf_Connect_LastSignIn.sh
@@ -4,6 +4,9 @@
 # Data Type: String
 # Input Type: Script
 #
+# Updated 03 APR 2023 - Changed return value from a string to epoch zero date to handle issue with 
+# smart computer groups expecting a date when a string was returned. - S. Rabbitt D. MacLaughlin
+#
 # Copyright (c) 2022 JAMF Software, LLC
 ##### Script starts here #####
 
@@ -13,7 +16,7 @@ currentUser=$(ls -l /dev/console | awk '/ / { print $3 }')
 #com.jamf.connect.state.plist location
 jamfConnectStateLocation=/Users/$currentUser/Library/Preferences/com.jamf.connect.state.plist
 
-LastSignIn=$(/usr/libexec/PlistBuddy -c "Print :LastSignIn" $jamfConnectStateLocation || echo "Does not exist")
+LastSignIn=$(/usr/libexec/PlistBuddy -c "Print :LastSignIn" $jamfConnectStateLocation || echo "1970-01-01 00:00:00")
 echo "LastSignIn"
 echo "<result>$LastSignIn</result>"
 
